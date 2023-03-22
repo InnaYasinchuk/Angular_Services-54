e1abea590a10a6381bd2bdc3b7d8e5a564df70ce
Inna Yasynchuk <i_bybyk@ukr.net>
2023-03-22 17:48:26 +0200

    solved the tasks

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { UserGroupService } from '../service/user-group.service';
+import { UsersService } from '../service/users.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+  constructor(public UserGroupService: UserGroupService, public UsersService: UsersService){}
+ newGroupName = '';
+ newName = '';
+ newStatus = '';
+ groupNameEdit() {
+    this.UserGroupService.groupName = this.newGroupName;
+    this.newGroupName = '';
+  }
+
+  nameEdit() {
+    this.UsersService.users[this.id].name = this.newName; 
+    this.newName = '';
+  }
+  statusEdit() {
+    this.UsersService.users[this.id].status = this.newStatus;
+    this.newStatus = '';
+  }
+
+  @Input() name = ''
+  @Input() status = ''
+  @Input() img = ''
+  @Input() id = 0
+}
